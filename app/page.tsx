99c0586f0a065c31ecc6a441f4fdd7276be55cad
Robert-Lexicon <robert.hedblad@lexicon.se>
2025-09-24 11:24:03 +0200

    Search example

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 //Next.js Form component - https://nextjs.org/docs/app/api-reference/components/form
 
+import Form from "next/form";
+
 export default function Home() {
   return (
     <section className="grid gap-4">
       <h2 className="text-xl font-bold text-center">Search</h2>
+      <Form action="/">
+        <label htmlFor="search">Search</label>
+        <input className="border" type="text" id="search" name="query" />
+        <label htmlFor="smt">Nåt annat</label>
+        <input className="border" type="text" id="smt" name="somethingelse" />
+        <button type="submit" className="cursor-pointer border p-2">
+          Search
+        </button>
+      </Form>
     </section>
   );
 }
