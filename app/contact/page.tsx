9ac20344300f2cf8423aeed14f831c04d47b6e4f
Robert-Lexicon <robert.hedblad@lexicon.se>
2025-09-24 11:23:43 +0200

    example with redirect + searchparams in server action

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,44 @@
 import Form from "next/form";
+import { redirect } from "next/navigation";
 
 // nodemailer + gmail - https://dev.to/emmanuel_xs/how-to-send-emails-for-free-in-nextjs-using-gmail-and-nodemailer-4i6e
 // resend - https://resend.com/docs/send-with-nextjs
 
-export default function Contact() {
+//server action (inline) - mutations
+async function contactAction(formData: FormData) {
+  "use server";
+  const email = formData.get("email");
+  const title = formData.get("title");
+  const message = formData.get("message");
+
+  //skicka till api eller annat...
+
+  console.log(email);
+
+  //redirect("/contact/thanks") //redirect to other page
+  redirect("/contact?status=error"); //redirect using search params
+}
+
+export default async function Contact({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+  const { status } = await searchParams;
   return (
     <section className="grid gap-4">
       <h2 className="text-xl font-bold text-center">Contact us</h2>
-      <Form className="grid gap-2" action="">
+      {status === "success" && (
+        <p className="text-green-600 text-center font-bold">
+          Thank you for contacting us!
+        </p>
+      )}
+      {status === "error" && (
+        <p className="text-red-600 text-center font-bold">
+          Oh no, appears we have an error...
+        </p>
+      )}
+      <Form className="grid gap-2" action={contactAction}>
         <label htmlFor="email">Your e-mail</label>
         <input
           className="border p-2"
@@ -28,6 +59,7 @@ export default function Contact() {
           placeholder="My subject"
           required
           minLength={4}
+          maxLength={99}
         />
 
         <label htmlFor="message">Message</label>
