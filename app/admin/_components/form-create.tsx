7b6af134bdb4c197a9446a9466efae08782718e3
Robert-Lexicon <robert.hedblad@lexicon.se>
2025-09-25 11:02:52 +0200

    useActionState example

--- app/admin/_components/form-create.tsx
+++ app/admin/_components/form-create.tsx
@@ -1,35 +1,56 @@
 "use client";
-import { createProduct } from "@/app/admin/_lib/actions";
+import { createProduct, createProductState } from "@/app/admin/_lib/actions";
 import { PlatziCategory } from "@/lib/types";
 import Form from "next/form";
 import Link from "next/link";
+import { useActionState } from "react";
 import { useFormStatus } from "react-dom";
 
 // https://nextjs.org/docs/app/getting-started/project-structure#route-groups-and-private-folders
 
 // a fancier button using formstatus to see if the server action is pending or not
-function SaveButton() {
-  const { pending } = useFormStatus();
-  return (
-    <button
-      className="bg-neutral-200 p-2 cursor-pointer"
-      type="submit"
-      disabled={pending}
-    >
-      {pending ? "Saving..." : "Save"}
-    </button>
-  );
-}
+// function SaveButton() {
+//   const { pending } = useFormStatus();
+//   return (
+//     <button
+//       className="bg-neutral-200 p-2 cursor-pointer"
+//       type="submit"
+//       disabled={pending}
+//     >
+//       {pending ? "Saving..." : "Save"}
+//     </button>
+//   );
+// }
+
+//const initialState = { message: "" };
 
 export default function FormCreate({
   categories,
 }: {
   categories: PlatziCategory[];
 }) {
+  const [state, formAction, isPending] = useActionState(
+    createProductState,
+    null
+  );
+
+  //client would be between formAction and createProductState
+
+  console.log(state);
   return (
-    <Form className="grid gap-2" action={createProduct}>
+    <Form
+      className="grid gap-2"
+      action={formAction}
+      key={JSON.stringify(state?.data)}
+    >
+      {state?.message && <p>{state.message}</p>}
       <label htmlFor="category">Category</label>
-      <select name="category" id="category" className="border p-2">
+      <select
+        name="category"
+        id="category"
+        className="border p-2"
+        defaultValue={state?.data.categoryId ?? categories[0].id}
+      >
         {categories.map((category) => (
           <option key={category.id} value={category.id}>
             {category.name}
@@ -47,6 +68,7 @@ export default function FormCreate({
         required
         minLength={4}
         maxLength={99}
+        defaultValue={state?.data.title ?? ""}
       />
 
       <label htmlFor="description">Description</label>
@@ -58,6 +80,7 @@ export default function FormCreate({
         placeholder="Product description"
         required
         minLength={10}
+        defaultValue={state?.data.description ?? ""}
       />
 
       <label htmlFor="price">Price</label>
@@ -70,6 +93,7 @@ export default function FormCreate({
         // min={0.50}
         // step={0.10}
         required
+        defaultValue={state?.data.price ?? ""}
       />
 
       <label htmlFor="imageUrl">Image url</label>
@@ -80,6 +104,7 @@ export default function FormCreate({
         name="imageUrl"
         placeholder="https://placehold.co/600x400"
         required
+        defaultValue={state?.data.images[0] ?? ""}
       />
 
       <div className="justify-self-end flex gap-2">
@@ -89,7 +114,13 @@ export default function FormCreate({
         >
           Cancel
         </Link>
-        <SaveButton />
+        <button
+          className="bg-neutral-200 p-2 cursor-pointer"
+          type="submit"
+          disabled={isPending}
+        >
+          {isPending ? "Saving..." : "Save"}
+        </button>
       </div>
     </Form>
   );
